f1bf2df6b0690d0617dae29e7c1871ecbc083ab4
MinsuKimhero <kmshero112@gmail.com>
2016-10-10 18:25:50 +1000

    test
    11

--- pages/studentPage.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include "../modules/session.php";
-	include "../modules/database.php";
-    include "../modules/sanitize.php";
-	include "studentHeader.php";
-    include "studentBody.php";
-	include "footer.php";
-?>
--- pages/teacherPage.php
+++
@@ -1,9 +0,0 @@
-<?php
-        include "../modules/session.php";
-		include "../modules/database.php";
-		include "../modules/sanitize.php";
-		include "headerPage.php";
-		include "teacherBody.php";
-		include "footer.php";
-?>
-
