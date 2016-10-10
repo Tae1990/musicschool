f609011369facded18fc015f04da78cf6a2af895
Tae1990 <canonipop@hotmail.com>
2016-10-10 17:55:50 +1000

    delete
    all and restart

--- pages/about.php
+++
@@ -1,8 +0,0 @@
-<?php
-        include "../modules/session.php";
-		include "../modules/database.php";
-		include "../modules/sanitize.php";
-		include "headerPage.php";
-		include "aboutBody.php";
-		include "footer.php";
-?>
