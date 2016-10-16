6c79e550370d5392a9321ebb33a56884076d7958
IX54 <christopheryin@hotmail.com>
2016-10-16 19:58:00 +1000

    Documents
    Documents Will be Added Here

--- pages/studentInstrumentHire.php
+++
@@ -1,8 +0,0 @@
-<?php
-        include "../modules/session.php";
-		include "../modules/database.php";
-		include "../modules/sanitize.php";
-		include "studentHeader.php";
-		include "instrumentHireBody.php";
-		include "footer.php";
-?>
