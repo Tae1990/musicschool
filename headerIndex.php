a8e5838278e47f8d0970ae8b696abe9d7373c3aa
Tae1990 <canonipop@hotmail.com>
2016-10-17 15:01:34 +1000

    navigation bar
    navigation bar for the main pages

---
+++ headerIndex.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="css/style.css">
+  
+ <title>QUT Music School</title>
+
+  <style>
+  <!-- already put the class in the css file -->
+  </style>
+
+</head>
+
+  <nav class="navbar navbar-default navbar-fixed-top">
+	<div class="container-fluid">
+	<div class="navbar-header">
+<!-- 
+   <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+   <span class="icon-bar"></span> 
+   <span class="icon-bar"></span>
+   <span class="icon-bar"></span>
+   </button>
+  
+ -->
+	  <a class="navbar-brand" href="index.php">QUT Music School</a>
+	 </div>
+	  <div class="collapse navbar-collapse" id="myNavbar">
+		  <ul class="nav navbar-nav navbar-right">
+			  <li><a href="index.php">HOME</a></li>
+			  <li><a href="#newsfeed">News</a></li>
+			  <li><a href="modules/lesson.php">Lesson</a></li>
+
+			  <li class="dropdown">
+				  <a class="dropdown-toggle" data-toggle="dropdown" href="#">MORE
+					<span class="caret"></span>
+				  </a>
+				  <ul class="dropdown-menu">
+					  <li><a href="modules/loginSessionIndex.php">Log in</a></li>
+					  <li><a href="modules/register.php">Register</a></li>
+					  <li><a href="pages/about_us.php">About us</a></li>
+					  <li><a href="pages/contact.php">Contact us</a></li>
+				  </ul>
+			  </li>
+		  </ul>
+	 </div>
+	</div>
+  </nav>
