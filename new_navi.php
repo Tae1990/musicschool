03c48d7e6a0b6d635ba901d32831e2629701c293
Tae1990 <canonipop@hotmail.com>
2016-10-19 10:35:24 +1000

    new navi bar
    changing layout by bootstrap that I made in release one

---
+++ new_navi.php
@@ -0,0 +1,199 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>QUT Music School</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../css/sb-admin.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+   
+
+    <!-- Custom Fonts -->
+   
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+           <div class="navbar-header">
+
+                <a class="navbar-brand" href="index.html">QUT Music school</a>
+            </div>
+            
+                
+
+            <!-- Top Menu Items -->
+            <ul class="nav navbar-right top-nav">  <!--이부분에서 프로필 설정하겠끔 -->
+
+               
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> 
+       
+<?php 
+ if (!isset($_SESSION['login']) and !isset($_SESSION['error']))
+ {       
+	echo(
+          "<div class=\"login\">
+           <form action=\"../modules/login.php\" method=\"post\">
+           <div class=\"form-group\">
+           <label for=\"userName\">User name</label>
+           <input type=\"text\" id=\"userName\" name=\"userName\" placeholder=\"Enter userName\" required>
+           </div>
+           <div class=\"form-group\">
+           <label for=\"password\">Password</label>
+           <input type=\"password\" id=\"password\" name=\"password\" placeholder=\"Enter password\">
+           </div>
+           <button type=\"submit\"> Login</button>
+           </form>
+           <form action=\"register_student.php\">
+           <input type=\"submit\" value=\"Register as student\">
+           </form>
+		   <form action=\"register_teacher.php\">
+           <input type=\"submit\" value=\"Register as teacher\">
+           </form>
+		   
+		  
+		  </div><!--login-->");
+ }
+ elseif ( isset($_SESSION['error']) )
+ {
+	 
+	
+	 echo("<div class=\"login\">
+           <form action=\"../modules/login.php\" method=\"post\">
+           <div class=\"form-group\">
+           <label for=\"userName\">User name</label>
+           <input type=\"text\" id=\"userName\" name=\"userName\" placeholder=\"Enter userName\" required>
+            </div>
+            <div class=\"form-group\">
+              <label for=\"password\">Password</label>
+              <input type=\"password\" id=\"password\" name=\"password\" placeholder=\"Enter password\">
+            </div>
+              <button type=\"submit\"> Login</button>
+              </form>
+              <form action=\"register_student.php\">
+           <input type=\"submit\" value=\"Register as student\">
+           </form>
+		   <form action=\"register_teacher.php\">
+           <input type=\"submit\" value=\"Register as teacher\">
+           </form>
+			  ".$_SESSION['error']."
+			</div><!--login-->");	
+			
+			
+	unset($_SESSION['error']);		  
+ }
+else
+{
+	echo ("<div class=\"login\">
+				 hello :)
+		         <form action=\"../modules/logout.php\" method=\"post\">". $_SESSION['userName'] . "
+				 <br>
+				 
+				 <button type=\"submit\"> Log out </button>
+				 </form>
+		         </div><!--login-->"); 
+           }
+?>	   
+ <b class="caret"></b></a>
+				  <ul class="dropdown-menu">
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
+                        </li>
+                        
+                        <li class="divider"></li>
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
+                        </li>
+                    </ul>
+                </li>
+				
+				
+            </ul>
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li class="active">
+                        <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> main 1</a>
+                    </li>
+                   
+                    <li>
+                        <a href="index.html"><i class="fa fa-fw fa-table"></i> main 2</a>
+                    </li>
+                    <li>
+                        <a href="index.html"><i class="fa fa-fw fa-edit"></i> main 3</a>
+                    </li>
+                    <li>
+                        <a href="index.html"><i class="fa fa-fw fa-desktop"></i> main 3</a>
+                    </li>
+                    <li>
+                        <a href="index.html"><i class="fa fa-fw fa-wrench"></i> main 4</a>
+                    </li>
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="demo" class="collapse">
+                            <li>
+                                <a href="#">Dropdown Item</a>
+                            </li>
+                            <li>
+                                <a href="#">Dropdown Item</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                  
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </nav>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+			test
+                <!-- Page Heading -->
+
+           
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+	
+	<!-- jQuery -->
+    <script src="../js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../js/bootstrap.min.js"></script>
+
+
+</body>
+
+</html>
